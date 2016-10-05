690d2e8b4f68eb14d7922d280ac44034de909d58
clement <clement@debian>
2016-10-05 12:41:29 +0200

    classe animateur (tacher) enlever le bouto

--- opisiame/src/opisiame/controller/Interface_authentificationController.java
+++ opisiame/src/opisiame/controller/Interface_authentificationController.java
@@ -5,10 +5,19 @@
  */
 package opisiame.controller;
 
+import java.awt.event.*;
+import java.io.*;
 import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 import java.sql.*;
+import java.util.ResourceBundle;
+import javafx.fxml.*;
+import javafx.fxml.Initializable;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.shape.*;
+import javafx.stage.*;
+import opisiame.database.*;
+import opisiame.model.*;
 
 /**
  * FXML Controller class
@@ -16,7 +25,15 @@ import java.sql.*;
  * @author clement
  */
 public class Interface_authentificationController implements Initializable {
-
+    @FXML
+    private TextField login_field;
+    @FXML
+    private PasswordField Passwd_field;
+    @FXML 
+    private Label Message_field;
+    @FXML
+    private Polygon Submit_field;
+    
     /**
      * Initializes the controller class.
      */
@@ -25,4 +42,18 @@ public class Interface_authentificationController implements Initializable {
         // TODO
     }    
     
+    public void Submit_passwd(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("opisiame.view.menu_admin.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setTitle("menu");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (IOException e) {
+
+        }
+    }
 }
