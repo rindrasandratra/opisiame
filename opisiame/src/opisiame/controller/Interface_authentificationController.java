386ae12bcf35d130fc935f5fccbc57dcbe26d4ce
rindrasandratra <rindras93@yahoo.fr>
2016-10-05 15:51:25 +0200

    Merge origin/master
    Conflicts:
    	opisiame/nbproject/project.properties
    	opisiame/src/opisiame/Opisiame.java

--- opisiame/src/opisiame/controller/Interface_authentificationController.java
+++ opisiame/src/opisiame/controller/Interface_authentificationController.java
@@ -5,10 +5,18 @@
  */
 package opisiame.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-import java.sql.*;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,12 +25,27 @@ import java.sql.*;
  */
 public class Interface_authentificationController implements Initializable {
 
+    @FXML
+    private AnchorPane content;
+
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void btn_connect() throws IOException {
+        Stage stage = (Stage) content.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/opisiame/view/liste_quize.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
