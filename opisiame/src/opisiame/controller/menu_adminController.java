e882aca93271453b9af4caba4cf5f454f8d62a4a
Audrey <Audrey@10.4.23.126>
2016-10-05 11:39:47 +0200

    menu_admin

---
+++ opisiame/src/opisiame/controller/menu_adminController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opisiame.controller;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Audrey
+ */
+public class menu_adminController implements Initializable{
+
+    @FXML
+ private void ClicBoutonEleves(ActionEvent event) {
+     // Button was clicked, do something...
+        System.out.println("blabla");;
+ }
+   
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
