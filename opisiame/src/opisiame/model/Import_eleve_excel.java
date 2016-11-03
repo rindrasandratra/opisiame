4320dc0331e9bcd2df3ea11a68618b043fafc829
clement <clement@debian>
2016-11-03 12:05:22 +0100

    Code qui marche pas commenté

--- opisiame/src/opisiame/model/Import_eleve_excel.java
+++ opisiame/src/opisiame/model/Import_eleve_excel.java
@@ -7,17 +7,17 @@ package opisiame.model;
 
 import java.io.*;
 import java.util.Iterator;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
+/*import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Row;*/
 import java.util.*;
 import java.util.Collection.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+/*import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;*/
 
 /**
  *
@@ -31,15 +31,15 @@ public class Import_eleve_excel {
     private List<String> Liste_filiere = new ArrayList<>();
     private List<Integer> Liste_annee = new ArrayList<>();
     private int nb_element; //nombre d'élément dans le fichier excel
-    private XSSFWorkbook classeur;
+    /*private XSSFWorkbook classeur;*/
 
     public Import_eleve_excel() throws IOException {
         System.out.print("appelle constructeur import eleve excel \n");
         nb_element = 0;
-        ouverture_fichier();
+       /* ouverture_fichier();*/
         System.out.print("je suppose que ça doit marcher vu qu'il n'y a pas d'erreur \n");
     }
-
+/*
     private void ouverture_fichier() throws IOException {
         InputStream is = new FileInputStream("/home/clement/Lycée/2016_2017/plp/test.xlsx");
         OPCPackage opc;
@@ -50,6 +50,6 @@ public class Import_eleve_excel {
             Logger.getLogger(Import_eleve_excel.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-    }
+    }*/
 
 };
