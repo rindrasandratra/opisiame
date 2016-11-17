b3c067bb8e504e8d0877044e2dd6df86e0a2f01d
clement <clement@debian>
2016-11-17 11:37:18 +0100

    lecture fichier excel eleve ok + selection excel eleve ok (à tester tout de même avec un fichier *.xls)

--- opisiame/src/opisiame/controller/gestion_eleve/Import_excelController.java
+++ opisiame/src/opisiame/controller/gestion_eleve/Import_excelController.java
@@ -5,13 +5,14 @@
  */
 package opisiame.controller.gestion_eleve;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import opisiame.model.Import_eleve_excel;
 
@@ -26,7 +27,7 @@ public class Import_excelController implements Initializable {
     private TextField zone_url;
     @FXML
     private AnchorPane content;
-    
+
     private String adresse;
 
     /**
@@ -46,11 +47,26 @@ public class Import_excelController implements Initializable {
         fermer();
     }
 
-    
     @FXML
     private void fermer() throws IOException {
         Stage stage = (Stage) content.getScene().getWindow();
         stage.close();
+    }
+
+    public void explorateur() {
+        final FileChooser dialog = new FileChooser();
+        dialog.setTitle("Choisir le fichier");
+        dialog.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Excel file", "*.xlsx", "*.xls")
+        );
+        dialog.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        //recuperation de l'adresse du fichier
+        File file = dialog.showOpenDialog(content.getScene().getWindow());
+        if (file != null) {
+            zone_url.setText(file.toString());
+        }
+
     }
 
 }
