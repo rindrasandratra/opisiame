3bd49523a556f1903de2baeb2cf9d12950a80b72
Audrey <Audrey@172.16.1.68>
2017-01-05 09:40:28 +0100

    affichage résultats par élève

--- opisiame/src/opisiame/controller/gestion_resultat/Resultat_par_eleveController.java
+++ opisiame/src/opisiame/controller/gestion_resultat/Resultat_par_eleveController.java
@@ -7,10 +7,8 @@ package opisiame.controller.gestion_resultat;
 
 import java.io.IOException;
 import java.net.URL;
-//import java.util.*;
+import java.util.*;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.*;
@@ -24,6 +22,7 @@ import opisiame.model.Eleve;
 import opisiame.model.Question;
 import opisiame.model.Reponse;
 import opisiame.model.Reponse_eleve_quiz;
+import opisiame.model.Resultat_eleve_comp;
 
 /**
  * FXML Controller class
@@ -35,6 +34,7 @@ public class Resultat_par_eleveController implements Initializable {
     Resultat_dao resultat_dao = new Resultat_dao();
     Question_dao question_dao = new Question_dao();
     Reponse_dao reponse_dao = new Reponse_dao();
+    Sous_comp_dao sous_comp_dao = new Sous_comp_dao();
 
     @FXML
     private GridPane content;
@@ -51,32 +51,34 @@ public class Resultat_par_eleveController implements Initializable {
     @FXML
     private Tab onglet_q;
     @FXML
-    private TableView<Reponse_eleve_quiz> tab_comp;
+    private TableView<Resultat_eleve_comp> tab_comp;
     @FXML
     private TableView<Reponse_eleve_quiz> tab_question;
     @FXML
-    private TableColumn<Reponse_eleve_quiz, Integer> num_question;
+    private TableColumn<Reponse_eleve_quiz, String> question;
     @FXML
     private TableColumn<Reponse_eleve_quiz, String> c_bonne_r;
     @FXML
     private TableColumn<Reponse_eleve_quiz, String> c_r_eleve;
     @FXML
-    private TableColumn<Reponse_eleve_quiz, String> c_comp;
+    private TableColumn<Resultat_eleve_comp, String> c_comp;
     @FXML
-    private TableColumn<Reponse_eleve_quiz, String> c_pourcent;
+    private TableColumn<Resultat_eleve_comp, Integer> c_pourcent;
 
     int quiz_id;
     int participation_id;
     int nbre_questions;
+    int nbre_rep_eleve;
     int nbre_bonnes_rep;
+    float note = 0;
     String date_quiz;
     String nom_quiz;
-    private ObservableList<Eleve> liste_eleves;
-    //private ObservableList
-    private ArrayList<Question> liste_questions;
-    private ArrayList<Reponse> liste_reponses;
-    private ArrayList<Reponse> liste_reponses_eleve;
-    private ObservableList<Reponse_eleve_quiz> a_afficher = FXCollections.observableArrayList();
+    private ObservableList<Eleve> liste_eleves = FXCollections.observableArrayList();
+    private ArrayList<Question> liste_questions = new ArrayList<>();
+    private ArrayList<Reponse> liste_reponses = new ArrayList<>();
+    private ArrayList<Reponse> liste_reponses_eleve = new ArrayList<>();
+    private ObservableList<Reponse_eleve_quiz> a_afficher_1 = FXCollections.observableArrayList();
+    private ObservableList<Resultat_eleve_comp> a_afficher_2 = FXCollections.observableArrayList();
 
     public void setId(int id) {
         quiz_id = id;
@@ -91,9 +93,12 @@ public class Resultat_par_eleveController implements Initializable {
 
         nbre_bonnes_rep = 0;
 
-        num_question.setCellValueFactory(new PropertyValueFactory<Reponse_eleve_quiz, Integer>("num_ques"));
+        question.setCellValueFactory(new PropertyValueFactory<Reponse_eleve_quiz, String>("lib_ques"));
         c_bonne_r.setCellValueFactory(new PropertyValueFactory<Reponse_eleve_quiz, String>("rep_quiz"));
         c_r_eleve.setCellValueFactory(new PropertyValueFactory<Reponse_eleve_quiz, String>("rep_eleve"));
+
+        c_comp.setCellValueFactory(new PropertyValueFactory<Resultat_eleve_comp, String>("nom_comp"));
+        c_pourcent.setCellValueFactory(new PropertyValueFactory<Resultat_eleve_comp, Integer>("pourcentage"));
 
         try {
             Connection connection = Connection_db.getDatabase();
@@ -127,15 +132,29 @@ public class Resultat_par_eleveController implements Initializable {
 
     @FXML
     public void BtnValider() throws IOException {
-        
-        tab_question.getItems().clear();
-        tab_question.setItems(Reponses());        
 
-        double note = (nbre_bonnes_rep / (float)nbre_questions) * 20.0;
-        label_note.setText("Note : " + note + "/20");
+        //réinitialisation des différents éléments
+        tab_comp.getItems().clear();
+        tab_question.getItems().clear();
+        a_afficher_1.clear();
+        a_afficher_2.clear();
+        nbre_bonnes_rep = 0;
+        nbre_questions = 0;
+        nbre_rep_eleve = 0;
+        note = 0;
+        label_note.setText("Note : ");
+
+        Reponses();
+        tab_question.setItems(a_afficher_1); //on remplit le premier tableau
+        tab_comp.setItems(a_afficher_2);//on remplit le deuxieme tableau
+
+        //affichage de la note de l'élève
+        note = (float) ((nbre_bonnes_rep / (float) nbre_questions) * 20.0);
+        label_note.setText("Note : " + note + " / 20");
     }
 
-    public ObservableList<Reponse_eleve_quiz> Reponses() {
+    public void Reponses() {
+
         //récupération du numéro éudiant (part_id), et du participation_id
         try {
             Connection connection = Connection_db.getDatabase();
@@ -151,45 +170,94 @@ public class Resultat_par_eleveController implements Initializable {
             ex.printStackTrace();
         }
 
+        /*
+        **
+        **REMPLISSAGE DES TABLEAUX
+        **
+        **/
         //récupérer les questions / réponses / réponses de l'élève correspondant au quiz
         // 1 - récupérer les questions du quiz
+        liste_questions.clear();
         liste_questions = question_dao.get_questions_by_quiz(quiz_id);
         nbre_questions = liste_questions.size();
+
+        //récupération des réponses de l'élève au quiz
+        liste_reponses_eleve.clear();
+        liste_reponses_eleve = reponse_dao.get_reponses_eleve(participation_id);
+        nbre_rep_eleve = liste_reponses_eleve.size();
+
+        //récupération de la liste des compétences du quiz  
+        for (int k = 0; k < liste_questions.size(); k++) {
+            String n = resultat_dao.get_competence(liste_questions.get(k).getSous_comp_id()); //nom de la competence
+            Resultat_eleve_comp r = new Resultat_eleve_comp(n, 0, 0);
+            //ajouter la comp si elle n'appartient pas encore à la liste
+            boolean check = list_contains(a_afficher_2, r);
+            if (!check) {
+                a_afficher_2.add(r);
+            }
+        }
+
+        //récup de la bonne réponse et de la réponse de l'élève à la question (dans afficher)
         for (int i = 0; i < nbre_questions; i++) {
 
             Reponse_eleve_quiz afficher = new Reponse_eleve_quiz();
             afficher.setNum_ques(Integer.valueOf(i + 1));
+            afficher.setLib_ques(liste_questions.get(i).getLibelle());
 
-            //recherche des réponses de l'élève
-            liste_reponses_eleve = reponse_dao.get_reponses_eleve(participation_id);
+            //récupération de l'indice de la compétence correspondante dans la liste a_afficher_2
+            int indice = 0;
+            String comp = sous_comp_dao.get_comp(liste_questions.get(i).getSous_comp_id());
+            while (indice < a_afficher_2.size() && !a_afficher_2.get(indice).getNom_comp().equals(comp)) {
+                indice++;
+            }
+            a_afficher_2.get(indice).setNbre_questions(a_afficher_2.get(indice).getNbre_questions().intValue() + 1);
 
             //récupération de toutes les réponses des questions du quiz
-            char nom_question = 'a';
+            char nom_question = 'A';
             int question = liste_questions.get(i).getId();
             liste_reponses = reponse_dao.get_reponses_by_quest(question);
+
+            //initialisation
+            afficher.setRep_eleve("");
 
             //recherche de la bonne reponse
             for (int j = 0; j < 4; j++) {
 
                 if (liste_reponses.get(j).getIs_bonne_reponse() == 1) {
-                    afficher.setRep_quiz(Character.toString(nom_question));
+                    afficher.setRep_quiz(Character.toString(nom_question)); //si cette réponse est la bonne, on garde son libelle (A, B, C ou D)
                 }
 
-                if (liste_reponses.get(j).getId().equals(liste_reponses_eleve.get(i).getId())) {
-                    afficher.setRep_eleve(Character.toString(nom_question));
+                //recherche de la réponse de l'élève
+                for (int g = 0; g < nbre_rep_eleve; g++) {
+                    if (liste_reponses.get(j).getId().equals(liste_reponses_eleve.get(g).getId())) {
+                        afficher.setRep_eleve(Character.toString(nom_question)); //si l'élève a choisi cette reponse, on garde son libelle (A, B, C ou D)e
+                    }
                 }
-               
-                nom_question++;
+                
+                nom_question++; //passage à la réponse (A, B, C, D) suivante
             }
-            
+
+            //si l'élève n'a pas répondu à la question, on l'indique
+            if (afficher.getRep_eleve().equals("")) {
+                afficher.setRep_eleve("-"); 
+                System.out.println(afficher.getRep_eleve());
+            }
+
+            //si l'élève a bien repondu, on lui ajoute un point
             if (afficher.getRep_eleve().equals(afficher.getRep_quiz())) {
-                nbre_bonnes_rep = nbre_bonnes_rep+1;
-            }     
-            
-            a_afficher.add(afficher);
+                nbre_bonnes_rep = nbre_bonnes_rep + 1; 
+                a_afficher_2.get(indice).setPourcentage((float) (a_afficher_2.get(indice).getPourcentage().intValue() + 1));
+            }
+
+            a_afficher_1.add(afficher);
         }
 
-        return a_afficher;
+        //calcul de pourcentage de réussite à la compétence
+        for (int i = 0; i < a_afficher_2.size(); i++) {
+            float p = a_afficher_2.get(i).getPourcentage().floatValue() / a_afficher_2.get(i).getNbre_questions().floatValue();
+            a_afficher_2.get(i).setPourcentage(p * 100);
+        }
+
     }
 
     @FXML
@@ -198,4 +266,18 @@ public class Resultat_par_eleveController implements Initializable {
         stage.close();
     }
 
+    public boolean list_contains(ObservableList<Resultat_eleve_comp> L, Resultat_eleve_comp o) {
+        boolean check = false;
+        String comp = o.getNom_comp();
+        for (int i = 0; i < L.size(); i++) {
+            if (L.get(i).getNom_comp().equals(comp)) {
+                check = true;
+            } else {
+                check = false;
+            }
+        }
+        return check;
+    }
+
 }
+
--- opisiame/src/opisiame/model/Reponse_eleve_quiz.java
+++ opisiame/src/opisiame/model/Reponse_eleve_quiz.java
@@ -16,6 +16,7 @@ import javafx.beans.property.StringProperty;
  */
 public class Reponse_eleve_quiz {
 
+    private StringProperty lib_ques = new SimpleStringProperty();
     private IntegerProperty num_ques = new SimpleIntegerProperty();
     private StringProperty rep_quiz = new SimpleStringProperty();
     private StringProperty rep_eleve = new SimpleStringProperty();
@@ -23,16 +24,26 @@ public class Reponse_eleve_quiz {
     public Reponse_eleve_quiz() {
     }
 
-    public Reponse_eleve_quiz(IntegerProperty num, StringProperty rep_q, StringProperty rep_e) {
+    public Reponse_eleve_quiz(StringProperty lib_ques, IntegerProperty num, StringProperty rep_q, StringProperty rep_e) {
+        this.lib_ques = lib_ques;
         this.num_ques = num;
         this.rep_quiz = rep_q;
         this.rep_eleve = rep_e;
     }
-    
-     public Reponse_eleve_quiz(Integer num, String rep_q, String rep_e) {
+
+    public Reponse_eleve_quiz(String lib, Integer num, String rep_q, String rep_e) {
+        this.lib_ques.set(lib);
         this.num_ques.set(num);
         this.rep_quiz.set(rep_q);
         this.rep_eleve.set(rep_e);
+    }
+
+    public String getLib_ques() {
+        return this.lib_ques.get();
+    }
+
+    public void setLib_ques(String lib_ques) {
+        this.lib_ques.set(lib_ques);
     }
 
     public Integer getNum_ques() {
@@ -58,31 +69,5 @@ public class Reponse_eleve_quiz {
     public void setRep_eleve(String rep_eleve) {
         this.rep_eleve.set(rep_eleve);
     }
-     
-      
-//    public Integer getnum_question() {
-//        return num_ques.get();
-//    }
-//
-//    public void setnum_question(Integer id) {
-//        this.num_ques.set(id);
-//    }
-//
-//    public String getrep_q() {
-//        return rep_quiz.get();
-//    }
-//
-//    public void setrep_q(String nom) {
-//        this.rep_quiz.set(nom);
-//    }
-//
-//    public String getrep_e() {
-//        return rep_eleve.get();
-//    }
-//
-//    public void setrep_e(String lg) {
-//        this.rep_eleve.setValue(lg);
-//    }
-
 
 }
